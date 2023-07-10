775524c567119f7d194f6715b16ed1990483ba6d
Kaue Andrade Padilha <kauemassola900@gmail.com>
2023-07-10 13:48:37 -0300

    creating first component

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,15 @@
+//Images
+import Image from 'next/image'
+//Css
+import styles from './logo.module.css'
+
+const Logo = () => {
+    return (
+        <div className={styles.logo}>
+            <Image src='/logo.svg' width={60} height={60} />
+            <h1>SteamDois</h1>
+        </div>
+    )
+}
+
+export default Logo
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+//Css
+import styles from "./navbar.module.css"
+//Icons
+import { BsCart2 } from "react-icons/bs"
+//Components
+import Logo from '@/components/logo/logo'
+
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <Logo />
+            <input type="text" />
+            <BsCart2 size={40} />
+        </nav>
+    )
+}
+
+export default Navbar
